d07de673056406f68d5febd217da117c65148872
Tapsi <blackcat.myako@gmail.com>
2013-07-24 11:06:31 +0200

    travis #3

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,21 @@
 module.exports = function(grunt) {
-
-  grunt.initConfig({
-    	
-		qunit-minimalist: {
+	
+	var VERSION = "0-9-0";
+	
+	var uglyFiles = {};
+	uglyFiles['dist/isMobile_'+VERSION+'_min.js'] = [ 'dist/jsFlow_'+VERSION+'.js' ];
+	
+	var cfg = {
+    
+    copy: {
+  		main: {
+   		  files: [
+					{src: ['lib/jsFlow.js'], dest: 'dist/jsFlow_'+VERSION+'.js', filter: 'isFile' }
+				]
+			}
+		},
+    
+		'qunit-minimalist': {
         options: {
             parameters: 'noglobals=true',
             phantomOptions: {
@@ -16,14 +29,15 @@ module.exports = function(grunt) {
 		
     uglify: {
       my_target: {
-        files: {
-          'dist/isMobile_0-9-0_min.js': ['lib/jsFlow.js']
-        }
+        files: uglyFiles
       }
     }
 
-  });
+  };
 
+  grunt.initConfig( cfg );
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-qunit-minimalist');
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
