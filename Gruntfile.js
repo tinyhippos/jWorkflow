094d4aee53327d1609f800ebf7fe20b915bc8a4d
Tapsi <blackcat.myako@gmail.com>
2013-07-24 11:32:06 +0200

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,46 +1,29 @@
 module.exports = function(grunt) {
-	
-	var VERSION = "0-9-0";
-	
-	var uglyFiles = {};
-	uglyFiles['dist/isMobile_'+VERSION+'_min.js'] = [ 'dist/jsFlow_'+VERSION+'.js' ];
-	
-	var cfg = {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
     
-    copy: {
-  		main: {
-   		  files: [
-					{src: ['lib/jsFlow.js'], dest: 'dist/jsFlow_'+VERSION+'.js', filter: 'isFile' }
-				]
-			}
-		},
-    
-		'qunit-minimalist': {
-        options: {
-            parameters: 'noglobals=true',
-            phantomOptions: {
-                '--web-security': false
-            }
-        },
-        all: {
-            src: [path.join('test', 'test.html')]
-        }
-    }
-		
     uglify: {
-      my_target: {
-        files: uglyFiles
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'dist/<%= pkg.name %>.min.js'
       }
-    }
+    },
+    
+    qunit: {
+    	all: ['test/*.html']
+  	}
+  });
 
-  };
-
-  grunt.initConfig( cfg );
-
-  grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-qunit-minimalist');
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['uglify', 'qunit-minimalist']);
+  // Default task(s).
+  grunt.registerTask('default', ['uglify','qunit']);
 
 };
